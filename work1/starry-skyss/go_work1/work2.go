47745504bd2103c46776f74cdf390c05d66994b4
star <1768447040@qq.com>
2023-09-07 20:02:24 +0800

    refactor: 对几个不足进行了完善

--- work1/starry-skyss/go_work1/work2.go
+++ work1/starry-skyss/go_work1/work2.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	var arr [10]int
 	var min int
-	count := 0
+	//count := 0
 
 	//fmt.Scanln(&arr[0], &arr[1], &arr[2], &arr[3], &arr[4], &arr[5], &arr[6], &arr[7], &arr[8], &arr[9])
 	for i := 0; i < 10; i++ {
@@ -14,8 +14,8 @@ func main() {
 	fmt.Scanf("%d", &min)
 	for i := 0; i < 10; i++ {
 		if arr[i] <= (min + 30) {
-			count++
+			//count++
 		}
 	}
-	fmt.Println(count)
+	fmt.Println(len(arr))
 }
