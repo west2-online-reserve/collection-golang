9a668fe00c1e18e4669aeb9363f1dda6f106e5a4
bubble <140199590+bubbbbbble@users.noreply.github.com>
2023-09-08 19:34:55 +0800

    Update luoguP5737.go

--- work1/bubbbbbble/luoguP5737.go
+++ work1/bubbbbbble/luoguP5737.go
@@ -1,23 +1,21 @@
 package main
 
 import "fmt"
-func isprime(x int)bool {
-	if (x==1)||(x==2) {
-		return false
-	} else{
-		for i:=2;i*i<x;i++{
-			if(x%i==0)return false
-		}
-		return true
-	}
-}
+
 func main() {
-	var x int
-	fmt.Scanf("%d",&x)
-	res=isprime(x)
-	if res{
-		fmt.Printf("Yes")
-	}else {
-		fmt.Printf("No")
+	var x, y int
+	var a [1000]int
+	cnt := 0
+	fmt.Scanf("%d%d", &x, &y)
+	for i := x; i <= y; i++ {
+		if (i%4 == 0 && i%100 != 0) || (i%400 == 0) {
+			a[cnt] = i
+			cnt++
+		}
+	}
+	fmt.Printf("%d\n", cnt)
+	for i := 0; i < cnt; i++ {
+		fmt.Printf("%d ", a[i])
 	}
 }
+
