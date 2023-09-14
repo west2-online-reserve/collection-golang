b18710ee6333179383b525c0c7b2380006451ebb
bubbbbbbble <2860120176@qq.com>
2023-09-14 18:50:53 +0800

    fix

--- work2/bubbbbbble/1.go
+++ work2/bubbbbbble/1.go
@@ -3,14 +3,25 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"os"
 	"regexp"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/antchfx/htmlquery"
 	"golang.org/x/net/html"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
+
+var DB *gorm.DB
+
+type Notice struct {
+	From    string
+	Title   string
+	Date    string
+	Details string
+}
 
 func getHtml(url string) (top *html.Node) {
 	client := &http.Client{}
@@ -30,76 +41,76 @@ func getHtml(url string) (top *html.Node) {
 	}
 	return top
 }
-func getContent(doc *html.Node) bool { //获取一个通知的内容
-	lilist, err := htmlquery.QueryAll(doc, "//li[@class='clearfloat']")
-	if err != nil {
-		fmt.Println(err)
-	}
-	for _, li := range lilist {
-		from, title, time, details, flag := getDetails(li) //flag用来判断时间是否符合要求
-		if !flag {
-			return false //此时时间已经不符合，返回false
+func getLink(startPage int, linkChan chan string, resChan chan bool) { //获取通知详情页的链接
+	for page := startPage; ; page++ {
+		url := "https://info22.fzu.edu.cn/lm_list.jsp?totalpage=955&PAGENUM=" + strconv.Itoa(page) + "&wbtreeid=1460" //观察发现不同页码下url只有PAGENUM有变化
+		doc := getHtml(url)
+		lilist, err := htmlquery.QueryAll(doc, "//li[@class='clearfloat']")
+		if err != nil {
+			fmt.Println(err)
 		}
-		storeInfo(from, title, time, details)
+		for _, li := range lilist {
+			linkNode := htmlquery.FindOne(li, "//a[2]")
+			link := "https://info22.fzu.edu.cn/" + htmlquery.SelectAttr(linkNode, "href")
+			linkChan <- link
+		}
+		if len(resChan) != 0 { //找到截至时间就关闭管道
+			close(linkChan)
+			break
+		}
 	}
-	return true
-}
-
-// 传入数据并导入文件
-func storeInfo(from string, title string, time string, details string) {
-	fileName := "notice.txt"
-	file, err := os.OpenFile(fileName, os.O_CREATE|os.O_APPEND, 0644)
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer file.Close()
-	fmt.Fprintln(file, from, title, time)
-	fmt.Fprintln(file, details)
-	fmt.Fprintln(file, "________________________________________________________________________________________________")
-
 }
 
 // 向通知具体内容的链接发起请求
-func getDetails(node *html.Node) (string, string, string, string, bool) {
-	linkNode := htmlquery.FindOne(node, "//a[2]")
-	link := "https://info22.fzu.edu.cn/" + htmlquery.SelectAttr(linkNode, "href")
-	html := getHtml(link)
-	fromNode := htmlquery.FindOne(html, "//h3[@class='fl']")
-	titleNode := htmlquery.FindOne(html, "//div[@class='conth1']")
-	timeNode := htmlquery.FindOne(html, "//div[@class='conthsj']")
-	detailsNode := htmlquery.FindOne(html, "//div[@class='v_news_content']")
-	from := htmlquery.InnerText(fromNode)
-	title := htmlquery.InnerText(titleNode)
-	time := htmlquery.InnerText(timeNode)
-	dateRegex := regexp.MustCompile("\\d{4}-\\d{2}-\\d{2}")
-	trimmedTime := dateRegex.FindString(time) //正则过滤掉除时间以外的文字
-	var flag bool
+func getContent(linkChan chan string, resChan chan bool) {
 	startTime := "2020-01-01"
 	endTime := "2021-09-01"
-	if trimmedTime >= startTime && trimmedTime <= endTime {
-		flag = true
-	} else {
-		flag = false
+	for {
+		link := <-linkChan
+		html := getHtml(link)
+		fromNode := htmlquery.FindOne(html, "//h3[@class='fl']")
+		titleNode := htmlquery.FindOne(html, "//div[@class='conth1']")
+		dateNode := htmlquery.FindOne(html, "//div[@class='conthsj']")
+		detailsNode := htmlquery.FindOne(html, "//div[@class='v_news_content']")
+		from := htmlquery.InnerText(fromNode)
+		from = strings.TrimSpace(from) //过滤一下前后空格
+		title := htmlquery.InnerText(titleNode)
+		date := htmlquery.InnerText(dateNode)
+		dateRegex, _ := regexp.Compile("\\d{4}-\\d{2}-\\d{2}")
+		date = dateRegex.FindString(date) //正则过滤掉除时间以外的文字
+		if date < startTime || date > endTime {
+			resChan <- true
+			break
+		}
+		details := htmlquery.InnerText(detailsNode)
+		notice := Notice{from, title, date, details}
+		DB.Create(&notice)
 	}
-	details := htmlquery.InnerText(detailsNode)
-	return from, title, trimmedTime, details, flag
+}
+func init() {
+	dsn := "root:123456@tcp(127.0.0.1:3306)/mydata?charset=utf8mb4&parseTime=True&loc=Local" // 连接到数据库
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		fmt.Println(err)
+	}
+	DB = db
+	DB.AutoMigrate(&Notice{})
 }
 func main() {
 	start := time.Now()
-	startPage := 153                  //这个数字是2021-09-01时间的页码
-	for page := startPage; ; page++ { //获取一页通知的内容
-		url := "https://info22.fzu.edu.cn/lm_list.jsp?totalpage=954&PAGENUM=" + strconv.Itoa(page) + "&wbtreeid=1460" //观察发现不同页码下url只有PAGENUM有变化
-		doc := getHtml(url)                                                                                           //获取初始页html
-		sign := getContent(doc)
-		if sign {
-			fmt.Printf("第%d页已爬取完毕\n", page)
-		} else {
+	startPage := 154                   //这个数字是2021-09-01时间的页码
+	linkChan := make(chan string, 100) //这是通知详情链接的管道
+	resChan := make(chan bool, 4)      //判断协程是否都工作完毕
+	go getLink(startPage, linkChan, resChan)
+	for i := 0; i < 4; i++ {
+		go getContent(linkChan, resChan)
+	}
+	for {
+		if len(resChan) == 4 {
 			break
 		}
 	}
 	end := time.Now()
 	diff := end.Sub(start)
 	fmt.Println(diff)
-
-} //并发版以及导入数据库未解决，以及有些通知的内容会出现html内容
-//9m13.3077443s
+} //14.9156975s
