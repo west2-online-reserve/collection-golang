1fab016cbae9159d723ebf655157d795d31cda8e
abeeswarm <3524201453@qq.com>
2023-09-17 20:21:27 +0800

    fix:a bug

--- work1/alittleswarm/4.go
+++ work1/alittleswarm/4.go
@@ -15,7 +15,7 @@ func main() {
 	}
 }
 func isPrime(x int) bool {
-	for i := 2; i < int(math.Sqrt(float64(x))); i++ {
+	for i := 2; i <= int(math.Sqrt(float64(x))); i++ {
 		if x%i == 0 {
 			return false
 		}
