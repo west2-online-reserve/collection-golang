f65cac2408e6ba955e73a66987ffdbc1338ce32d
FantasyRL <1845983502@qq.com>
2023-10-02 12:22:37 +0800

    add:fixed work1

--- work1/FantasyRL/P5737.go
+++ work1/FantasyRL/P5737.go
@@ -5,15 +5,13 @@ import "fmt"
 func main() {
 	var x, y int
 	fmt.Scanf("%d%d", &x, &y)
-	t := y - x
-	var yr [1500]int //缓冲区
 	var ryr []int
 	var count int
-	for i := 0; i <= t; i++ {
-		yr[i] = x + i
-		if (yr[i]%4 == 0 && yr[i]%100 != 0) || yr[i]%400 == 0 { //(yr[i]%4 == 0 && yr[i]%100 != 0) || yr[i]%400 == 0
-			ryr = append(ryr, yr[i])
+	for i := x; i <= y; i++ {
+		if (i%4 == 0 && i%100 != 0) || i%400 == 0 { //(yr[i]%4 == 0 && yr[i]%100 != 0) || yr[i]%400 == 0
 			count++
+			ryr = append(ryr, i)
+
 		}
 	}
 	fmt.Println(count)
