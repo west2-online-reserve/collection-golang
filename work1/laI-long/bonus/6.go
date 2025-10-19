f65e34f31d7814e7e5de6a7ebe1bbcd9485aae1e
lai-long <3496629516@qq.com>
2025-10-19 21:44:38 +0800

    再次修改了Bonus1（6.go）

--- work1/laI-long/bonus/6.go
+++ work1/laI-long/bonus/6.go
@@ -6,21 +6,21 @@ import (
 )
 
 func main() {
-	file, err := os.Open("./ninenine.txt")
-	defer file.Close()
+	file, err := os.OpenFile("./work1/lai-long/bonus/ninenine.txt", os.O_WRONLY|os.O_CREATE, 0666)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	for i := 1; i <= 10; i++ {
-		for j := 1; j <= 10; j++ {
-			str := fmt.Sprintf("%d * %d = %d", i, j, i*j)
+	for i := 1; i < 10; i++ {
+		for j := 1; j <= i; j++ {
+			str := fmt.Sprintf("%d * %d = %d\t", j, i, i*j)
 			_, err := file.WriteString(str)
 			if err != nil {
 				fmt.Println(err)
 				return
 			}
 		}
+		_, err = file.WriteString("\n")
 	}
-
+	defer file.Close()
 }
